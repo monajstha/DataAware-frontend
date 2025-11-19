593ca587759f278c168555ddc51de6cc58971af3
monajstha <manojstha700@gmail.com>
2025-11-19 15:08:17 +0000

    fix: increase the axios timeout to match render free instance wake up time

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 
 export const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 10000, // Free instance of render takes time to load, hence the unusual amount
+  timeout: 20000, // API hosted on free instance of render, which takes time to load, hence the unusual amount for timeout
   headers: {
     "Content-Type": "application/json",
   },
